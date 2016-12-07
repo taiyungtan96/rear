f05b79cad3365214a40fa6769fa3eb1e403ccad6
taruc <taruc@PC92>
2016-12-07 12:45:52 +0800

    frareacva

---
+++ src/app1.java
@@ -0,0 +1,12 @@
+
+
+public class app1 {
+      public static void main (String[] args){
+            int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+                          }
+}
+}
